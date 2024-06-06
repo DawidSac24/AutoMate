2a43e5e16f173851e9b0f79aa2362413fef9aab9
DawidSac24 <dawidsac24@gmail.com>
2024-06-06 13:31:48 +0200

    Update min_max.cpp

--- min_max.cpp
+++ min_max.cpp
@@ -1,8 +1,6 @@
 //********************************************** LIBRAIRIES *******************************************************************
 #include "global.h"
 
-
-
 bool pion() {
   if (colonne_select == colonne_precedent + 2 || colonne_select == colonne_precedent + 1 &&
 }
