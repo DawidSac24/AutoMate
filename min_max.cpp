002ba3c50607c8afc73a631fe50712f9719f0850
DawidSac24 <dawidsac24@gmail.com>
2024-06-10 16:51:11 +0200

    Update min_max.cpp

--- min_max.cpp
+++ min_max.cpp
@@ -11,63 +11,90 @@ bool verif_dep() {
   Serial.print("ligne_precedent ");
   Serial.println(ligne_precedent);
 
-  switch (sequance) {
-    case JOUEUR_BLANC:
-      switch (pion_selectionne) {
-        case PION_B:
-          if (colonne_precedent == 1 && colonne_select == 3 && ligne_select == ligne_precedent)  // si le pion n'a pas encore été deplacé, il a avancé de 2 cases et na pas changé de ligne
-            return true;                                                                         // on envoi un pour dire que le deplacement est correct
-          else if (colonne_select == colonne_precedent + 1) {
-            if (ligne_select == ligne_precedent)
-              return true;                                                                          // on envoi un pour dire que le deplacement est correct
-            else if (ligne_select == ligne_precedent + 1 || ligne_select == ligne_precedent - 1) {  // Si le pion a tué un pion sur ca diagonale (1 case d'ecart)
-              if (elim_pion(colonne_select, ligne_select) == true)                                  // elimination du pion
-                return true;
-            } else
-              return false;  // le deplacement n'est pas correct -> on retourne 0
+  // switch (sequance) {
+    // case JOUEUR_BLANC:
+    switch (pion_selectionne) {
+      case PION_B:
+        if (colonne_precedent == 1 && colonne_select == 3 && ligne_select == ligne_precedent)  // si le pion n'a pas encore été deplacé, il a avancé de 2 cases et na pas changé de ligne
+          return true;                                                                         // on envoi un pour dire que le deplacement est correct
+        else if (colonne_select == colonne_precedent + 1) {
+          if (ligne_select == ligne_precedent)
+            return true;                                                                          // on envoi un pour dire que le deplacement est correct
+          else if (ligne_select == ligne_precedent + 1 || ligne_select == ligne_precedent - 1) {  // Si le pion a tué un pion sur ca diagonale (1 case d'ecart)
+            elim_pion(colonne_select, ligne_select);                                              // elimination du pion
+            return true;
           } else
             return false;  // le deplacement n'est pas correct -> on retourne 0
-          break;
-        case TOUR_B:
+        } else
+          return false;  // le deplacement n'est pas correct -> on retourne 0
+        break;
+
+      case TOUR_B:
+        if (ligne_select != ligne_precedent || colonne_select != colonne_precedent) {
+          return false;
+        } else {
+
           if (ligne_select == ligne_precedent) {
             if (colonne_select > colonne_precedent) {
-              for (colonne_test = colonne_precedent; colonne_test < colonne_select; colonne_test++) {
-                if (planche[colonne_select][ligne_select] != 0)
+              for (int colonne_test = colonne_precedent; colonne_test < colonne_select; colonne_test++) {
+                Serial.print("colonne test = ");
+                Serial.println(colonne_test);
+                if (planche[colonne_test][ligne_select] != 0)
                   return false;  // le deplacement n'est pas correct -> on retourne 0
               }
             } else if (colonne_select < colonne_precedent) {
-              for (colonne_test = colonne_precedent; colonne_test > colonne_select; colonne_test--) {
+              for (int colonne_test = colonne_precedent; colonne_test > colonne_select; colonne_test--) {
+                Serial.print("colonne test = ");
+                Serial.println(colonne_test);
+                if (planche[colonne_test][ligne_select] != 0)
+                  return false;  // le deplacement n'est pas correct -> on retourne 0
               }
             }
-            if (planche[colonne_select][ligne_select] != 0) {
-              if (elim_pion(colonne_select, ligne_select) == true)  // elimination du pion
-                return true;
-              else
-                return false;  // le deplacement n'est pas correct -> on retourne 0
+          } else if (colonne_select == colonne_precedent) {
+            if (ligne_select > ligne_precedent) {
+              for (int ligne_test = ligne_precedent; ligne_test < ligne_select; ligne_test++) {
+                Serial.print("ligne test = ");
+                Serial.println(ligne_test);
+                if (planche[colonne_select][ligne_test] != 0)
+                  return false;  // le deplacement n'est pas correct -> on retourne 0
+              }
+            } else if (ligne_select < ligne_precedent) {
+              for (int ligne_test = ligne_precedent; ligne_test > ligne_select; ligne_test--) {
+                Serial.print("ligne test = ");
+                Serial.println(ligne_test);
+                if (planche[colonne_select][ligne_test] != 0)
+                  return false;  // le deplacement n'est pas correct -> on retourne 0
+              }
             }
-          }
-      }
-      break;
-
-    case JOUEUR_NOIR:
-      switch (pion_selectionne) {
-        case PION_N:
-          if (colonne_precedent == 6 && colonne_select == 4 && ligne_select == ligne_precedent)  // si le pion n'a pas encore été deplacé, il a avancé de 2 cases et na pas changé de ligne
-            return true;                                                                         // on envoi un pour dire que le deplacement est correct
-          else if (colonne_select == colonne_precedent - 1) {
-            if (ligne_select == ligne_precedent)
-              return true;                                                                          // on envoi un pour dire que le deplacement est correct
-            else if (ligne_select == ligne_precedent + 1 || ligne_select == ligne_precedent - 1) {  // Si le pion a tué un pion sur ca diagonale (1 case d'ecart)
-              if (elim_pion(colonne_select, ligne_select) == true)                                  // elimination du pion
-                return true;
-              else
-                return false;  // le deplacement n'est pas correct -> on retourne 0
-            } else
-              return false;  // le deplacement n'est pas correct -> on retourne 0
           } else
             return false;  // le deplacement n'est pas correct -> on retourne 0
-          break;
-      }
-      break;
-  }
+
+          if (planche[colonne_select][ligne_select] != 0) {
+            elim_pion(colonne_select, ligne_select);  // elimination du pion
+          }
+          return true;
+        }
+        break;
+
+        //   break;
+
+        // case JOUEUR_NOIR:
+        // switch (pion_selectionne) {
+      case PION_N:
+        if (colonne_precedent == 6 && colonne_select == 4 && ligne_select == ligne_precedent)  // si le pion n'a pas encore été deplacé, il a avancé de 2 cases et na pas changé de ligne
+          return true;                                                                         // on envoi un pour dire que le deplacement est correct
+        else if (colonne_select == colonne_precedent - 1) {
+          if (ligne_select == ligne_precedent)
+            return true;                                                                          // on envoi un pour dire que le deplacement est correct
+          else if (ligne_select == ligne_precedent + 1 || ligne_select == ligne_precedent - 1) {  // Si le pion a tué un pion sur ca diagonale (1 case d'ecart)
+            elim_pion(colonne_select, ligne_select);                                              // elimination du pion
+            return true;
+          } else
+            return false;  // le deplacement n'est pas correct -> on retourne 0
+        } else
+          return false;  // le deplacement n'est pas correct -> on retourne 0
+        break;
+    }
+    // break;
+  // }
 }
